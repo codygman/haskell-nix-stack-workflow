deb0ea641992b4992c9a78709db71526634e974e
codygman <cody@codygman.dev>
2020-06-14 16:48:26 -0500

    add default.nix

---
+++ default.nix
@@ -0,0 +1,12 @@
+let
+  haskellNix = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz) {};
+  nixpkgsSrc = haskellNix.sources.nixpkgs-2003;
+  nixpkgsArgs = haskellNix.nixpkgsArgs;
+in
+{ pkgs ? import nixpkgsSrc nixpkgsArgs
+, haskellCompiler ? "ghc883"
+}:
+pkgs.haskell-nix.stackProject {
+  src = pkgs.haskell-nix.haskellLib.cleanGit { name = "myproj"; src = ./.; };
+  compiler-nix-name = haskellCompiler;
+}
