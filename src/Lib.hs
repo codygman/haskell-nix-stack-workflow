8086967ad84c4bfb0d08f7db2b625618d94e546d
codygman <cody@codygman.dev>
2021-07-03 17:08:11 -0500

    try using newer witch function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE TypeApplications #-}
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  ) where
+
+import           Data.ByteString               as BS
+import           Witch
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  -- Try using something from witch 0.3.3.0
+  print $ tryFrom @String @ByteString "foo"
